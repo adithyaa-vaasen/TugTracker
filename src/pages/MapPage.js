b9f72f77d71b454fe734e6b082cc25b56c041556
adithyaa-vaasen <avaasen@saltchukmariness.com>
2026-08-27 10:07:42 -0700

    Update MapPage.js
    Change in baselayer to remove watermarks

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -1587,8 +1587,8 @@ function MapPage() {
         <ZoomControl position="topright" />
         <ZoomTracker onZoom={setZoom} />
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"
-          attribution="&copy; OpenStreetMap contributors &copy; CARTO"
+          url="https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}"
+          attribution="Tiles &copy; Esri"
         />
         {baseLayer === "nautical" && (
           <WMSTileLayer
